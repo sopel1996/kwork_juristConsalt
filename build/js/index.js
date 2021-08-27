bf9c2ea03c03a41278a7ef2f09bdc6f86ff65584
Sopel1996 <sopel@live.ru>
2021-08-27 16:21:44 +0300

    done

--- build/js/index.js
+++ build/js/index.js
@@ -1,1 +1,14 @@
-for(let t of document.querySelectorAll('a[href*="#"]'))t.addEventListener("click",function(e){e.preventDefault();e=t.getAttribute("href").substr(1);document.getElementById(e).scrollIntoView({behavior:"smooth",block:"start"})});
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
